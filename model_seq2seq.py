5c393e62f7c1ad621b5568564053e84498b04e92
AdrianHsu <adrianhsu1995@gmail.com>
2018-05-09 21:58:26 +0800

    750000

--- model_seq2seq.py
+++ model_seq2seq.py
@@ -445,7 +445,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-lr', '--learning_rate', type=float, default=0.001) 
     parser.add_argument('-mi', '--min_counts', type=int, default=10) #50 -> 15000 words
-    parser.add_argument('-e', '--num_epochs', type=int, default=100)
+    parser.add_argument('-e', '--num_epochs', type=int, default=12)
     parser.add_argument('-b', '--batch_size', type=int, default=32)
     parser.add_argument('-t', '--test_mode', type=int, default=0)
     parser.add_argument('-d', '--num_display_steps', type=int, default=80)
