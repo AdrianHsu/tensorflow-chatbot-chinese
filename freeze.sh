be7f631c212011520a9aa73d4823aa58007f5643
AdrianHsu <adrianhsu1995@gmail.com>
2018-05-29 01:40:18 +0800

    409000 final

--- freeze.sh
+++ freeze.sh
@@ -1,2 +1,2 @@
 # ckpt-00000 should be modified
-python3 -m tensorflow.python.tools.freeze_graph --input_graph=load/test.pb --input_checkpoint=save/chatbot.ckpt-212000 --output_graph=load/frozen.pb --output_node_names=decoder/decoder_pred_eval --input_binary=true
+python3 -m tensorflow.python.tools.freeze_graph --input_graph=load/test.pb --input_checkpoint=save/chatbot.ckpt-409000 --output_graph=load/frozen.pb --output_node_names=decoder/decoder_pred_eval --input_binary=true
